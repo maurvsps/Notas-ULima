1c089bdcbfc7781687928f5e1812f5a1abc891aa
maurvsps <mvegas0403@gmail.com>
2026-06-25 21:24:19 -0500

    Refactor Firebase setup with auth and Firestore
    Updated Firebase initialization and added authentication and Firestore functionalities.

--- firebase.js
+++ firebase.js
@@ -1,22 +1,44 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBX0SLElkX2gryepvNrR1koAMBtRbcod_Y",
-    authDomain: "notasulima-e4984.firebaseapp.com",
-    projectId: "notasulima-e4984",
-    storageBucket: "notasulima-e4984.firebasestorage.app",
-    messagingSenderId: "725701401423",
-    appId: "1:725701401423:web:487d61b6570ea6b00c433a",
-    measurementId: "G-MN828NYVEM"
-  };
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-export { app };
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBX0SLElkX2gryepvNrR1koAMBtRbcod_Y",
+  authDomain: "notasulima-e4984.firebaseapp.com",
+  projectId: "notasulima-e4984",
+  storageBucket: "notasulima-e4984.firebasestorage.app",
+  messagingSenderId: "725701401423",
+  appId: "1:725701401423:web:487d61b6570ea6b00c433a",
+  measurementId: "G-MN828NYVEM"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export {
+  auth,
+  db,
+  provider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  doc,
+  getDoc,
+  setDoc
+};
