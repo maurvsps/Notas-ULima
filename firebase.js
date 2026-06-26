1b347008f54ff59a5c148e9af5b6cfcd46363416
maurvsps <mvegas0403@gmail.com>
2026-06-25 21:09:13 -0500

    Initialize Firebase app and analytics
    Added Firebase initialization and analytics setup.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBX0SLElkX2gryepvNrR1koAMBtRbcod_Y",
+    authDomain: "notasulima-e4984.firebaseapp.com",
+    projectId: "notasulima-e4984",
+    storageBucket: "notasulima-e4984.firebasestorage.app",
+    messagingSenderId: "725701401423",
+    appId: "1:725701401423:web:487d61b6570ea6b00c433a",
+    measurementId: "G-MN828NYVEM"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
